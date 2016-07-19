30db709f3a844b849a45fe81f94c61ad4f2419b3
Jonathan Dallas <jonathan.dallas@velir.com>
2016-07-19 09:12:13 -0400

    MGRV-42 - Gulp Setup - hooking up browsersync to reload for twig changes

--- styleguide/tools/gulp/tasks/browser-sync.js
+++ styleguide/tools/gulp/tasks/browser-sync.js
@@ -22,10 +22,12 @@ module.exports = function(config, env){
 
         // watch these files and reload the browser when they change
         files: [
-            config.dest + "/**",
+            config.rootSite + "/assets/**",
+            config.rootSite + "/styleguide/**",
             // prevent browser sync from reloading twice when the regular file (eg. index.js)
             // and the map file (eg. index.js.map) are generated
-            "!**/*.map"
+            "!**/*.map",
+
         ]
     };
 
