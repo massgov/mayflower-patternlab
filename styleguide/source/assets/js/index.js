f9a025675b3e47771a60c6603592f7dadc2561d0
Jonathan Dallas <jonathan.dallas@velir.com>
2016-08-01 14:35:30 -0400

    MGRV-78 - Header Mobile Nav - functionality for top level

--- styleguide/source/assets/js/index.js
+++ styleguide/source/assets/js/index.js
@@ -1,2 +1,3 @@
-import utilNav from "./modules/utilNav.js";
 import mainNav from "./modules/mainNav.js";
+import mobileNav from "./modules/mobileNav.js";
+import utilNav from "./modules/utilNav.js";
