217ba68402e2841aa7867e8279f4f7636de63802
Jonathan Dallas <jonathan.dallas@velir.com>
2016-08-15 19:37:08 -0400

    level0 - back2top - styling, js, html

--- styleguide/source/assets/js/index.js
+++ styleguide/source/assets/js/index.js
@@ -1,4 +1,5 @@
 import dropdown       from "./modules/dropdown.js";
+import back2top       from "./modules/back2top.js";
 import mainNav        from "./modules/mainNav.js";
 import mobileNav      from "./modules/mobileNav.js";
 import siteSettings   from "./modules/siteSettings.js";
