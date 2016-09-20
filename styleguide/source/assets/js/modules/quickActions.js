c723eefed52b4a45bc575debc823e5bcf5c9c41b
Jonathan Dallas <jonathan.dallas@velir.com>
2016-09-20 16:41:39 -0400

    Action Page - New Quick Actions Handlebars component

---
+++ styleguide/source/assets/js/modules/quickActions.js
@@ -0,0 +1,28 @@
+import getTemplate from "../helpers/getHandlebarTemplate.js";
+
+export default function (window,document,$,undefined) {
+
+  let compiledTemplate = getTemplate('quickactions'),
+      data = {
+        title: "Quick Actions",
+        items: []
+      }
+
+
+  $('.js-quickaction-link').each(function(){
+    let $link = $(this).find('a');
+
+    data.items.push({
+      href: $link.attr('href'),
+      external: $link.attr('target') === "_blank",
+      text: $.trim($link.text())
+    });
+  });
+
+  // prepend the quickaction list to the quick action components
+  let template = compiledTemplate(data);
+console.log(template);
+  $('.page-content').prepend(template);
+  $('.sidebar').prepend(template);
+
+}(window,document,jQuery);
