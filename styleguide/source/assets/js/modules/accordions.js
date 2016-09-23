77871c1df7b3ff45832a10a861870cfe64189fd4
Jonathan Dallas <jonathan.dallas@velir.com>
2016-09-23 10:25:40 -0400

    L1 - section links - finishing accordion styling

--- styleguide/source/assets/js/modules/accordions.js
+++ styleguide/source/assets/js/modules/accordions.js
@@ -1,13 +1,13 @@
 export default function (window,document,$,undefined) {
 
-  $('.js-accordion').each(function(){
+  $('.js-section-accordion').each(function(){
     let $el = $(this);
-console.log('$el',$el);
 
-    $el.find('.js-accordion-link').on('click',function(e){
-      e.preventDefault();
-      $el.toggleClass('is-open');
-console.log('clicked');
+    $el.find('.js-section-accordion-link').on('click',function(e){
+      if(window.innerWidth <= 480) {
+        e.preventDefault();
+        $el.toggleClass('is-open');
+      }
     })
   });
 
