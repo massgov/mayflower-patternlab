e176f62d62eeaf015ecd12ec1640cb46d37191a1
Jonathan Dallas <jonathan.dallas@velir.com>
2016-10-18 10:31:07 -0400

    MGVDU-210 - Header contact - added accordion feature

--- styleguide/source/assets/js/modules/accordions.js
+++ styleguide/source/assets/js/modules/accordions.js
@@ -1,14 +1,38 @@
 export default function (window,document,$,undefined) {
 
   $('.js-section-accordion').each(function(){
-    let $el = $(this);
+    let $el = $(this),
+        $link = $el.find('.js-section-accordion-link'),
+        $content = $el.find('.js-section-accordion-content'),
+        active = refreshValue();
 
-    $el.find('.js-section-accordion-link').on('click',function(e){
-      if(window.innerWidth <= 480) {
+    $link.on('click',function(e){
+      if(active) {
         e.preventDefault();
+        if($el.hasClass('is-open')){
+          $content.stop(true,true).slideUp();
+        } else {
+          $content.stop(true,true).slideDown();
+        }
         $el.toggleClass('is-open');
       }
     })
+
+    $(window).resize(function () {
+      let temp = refreshValue();
+
+      if(temp !== active && !temp) {
+        $content.removeAttr('style');
+        $el.removeClass('is-open');
+      }
+
+      active = temp;
+    }).resize();
   });
+
+  function refreshValue() {
+    let value = window.getComputedStyle(document.querySelector('.js-section-accordion'), ':before').getPropertyValue('content').replace(/\"/g, '');
+    return value === "false" ? false : true;
+  };
 
 }(window,document,jQuery);
