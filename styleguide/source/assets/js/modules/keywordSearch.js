b75315da515f8ac398e9526cadf6dddb26f76de0
gleroux02 <leroux@palantir.net>
2016-10-19 11:10:17 -0500

    MGRP-99: Change the search button on keyword search to input to appease drupal ajax.

--- styleguide/source/assets/js/modules/keywordSearch.js
+++ styleguide/source/assets/js/modules/keywordSearch.js
@@ -2,15 +2,16 @@ export default function (window,document,$,undefined) {
 
   $('.js-keyword-search').each(function(){
     let $el = $(this),
-        $form = $el.find('form');
+      $form = $el.find('form'),
+      $container = $el.parents('.ma__action-finder__container');
 
-    $form.on('submit',function(e){
+    $form.on('submit', function (e) {
       e.preventDefault();
-      $el.addClass('is-dirty')
+      $container.addClass('is-dirty');
     });
 
-    $form.on('reset',function(){
-      $el.removeClass('is-dirty')
+    $form.on('reset', function () {
+      $container.removeClass('is-dirty');
     });
   });
 
