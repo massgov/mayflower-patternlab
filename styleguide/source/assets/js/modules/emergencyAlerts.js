e011a6eb45bfe6689582d52b801be4638cf68ef7
Jonathan Dallas <jonathan.dallas@velir.com>
2016-12-07 16:00:47 -0500

    244 Emer Alert - JS to record the open/close state

---
+++ styleguide/source/assets/js/modules/emergencyAlerts.js
@@ -0,0 +1,30 @@
+import cookie   from "../helpers/cookies.js";
+
+export default function (window,document,$,undefined) {
+
+  $('.js-emergency-alerts').each(function(){
+    let $el = $(this),
+        open = true,
+        id = $el.data('id'),
+        cookieName = 'emergency-alerts' + id,
+        cookieValue = cookie.getCookie(cookieName);
+
+    if(typeof(cookieValue) != 'undefined' && cookieValue === 'false') {
+      // cookieValue is a string so we can't use the value directly
+      open = false;
+    }
+    if(open) {
+      // expand the menu
+      $el.find('.js-accordion-link').trigger('click');
+    }
+
+    $el.on('click','.js-accordion-link',function(){
+      // toggle the current state
+      open = !open;
+      // update open/close state cookie
+      cookie.setCookie(cookieName,open);
+    });
+
+  });
+
+}(window,document,jQuery);
