d2a7e6bbe53af047392bb7e3eafde60320f1865d
ygannett <yukikogannett@gmail.com>
2017-03-08 17:04:56 -0500

    modification to the review by Jonathan on Mar 7th.

--- styleguide/source/assets/js/modules/emergencyAlerts.js
+++ styleguide/source/assets/js/modules/emergencyAlerts.js
@@ -1,18 +1,21 @@
 import cookie   from "../helpers/cookies.js";
 
 export default function (window,document,$,undefined) {
-
   $('.js-emergency-alerts').each(function(){
     let $el = $(this),
         open = true,
         id = $el.data('id'),
         cookieName = 'emergency-alerts' + id,
         cookieValue = cookie.getCookie(cookieName),
-        $buttonState = $(this).find('.ma__button-alert').attr('aria-expanded');
+        $button = $el.find('.js-accordion-link button'),
+        // Add an aria-expanded attribute with its default value, false. to the <button>
+        $buttonState = $button.attr('aria-expanded', !open);
 
     if(typeof(cookieValue) != 'undefined' && cookieValue === 'false') {
       // cookieValue is a string so we can't use the value directly
       open = false;
+
+      $button.attr('aria-expanded', open);
     }
     if(open) {
       // expand the menu
@@ -25,10 +28,18 @@ export default function (window,document,$,undefined) {
       // update open/close state cookie
       // leave off third argument to make it expire on session
       cookie.setCookie(cookieName,open);
-      // change the state of aria-expanded
-     $buttonState === 'false' ? $buttonState = 'true' : $buttonState = 'false';
-     $(this).find('.ma__button-alert').attr('aria-expanded',$buttonState);
     });
+
+    $button.trigger('click');
+    $button.on('click', function(e) {
+      e.preventDefault();
+
+      open = !open;
+      // change the state of aria-expanded
+      $buttonState = $buttonState === open ? !open : open;
+      $(this).attr('aria-expanded', $buttonState);
+    });
+
 
   });
 
