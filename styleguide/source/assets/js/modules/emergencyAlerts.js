99c883d90be0a94303f65b74c3a3e8d7c8182f3a
Jonathan Dallas <jonathan.dallas@velir.com>
2017-03-10 16:38:41 -0500

    DP-1893 - Emer Alerts - Fixing logic of expand collapse button

--- styleguide/source/assets/js/modules/emergencyAlerts.js
+++ styleguide/source/assets/js/modules/emergencyAlerts.js
@@ -1,6 +1,10 @@
 import cookie   from "../helpers/cookies.js";
 
 export default function (window,document,$,undefined) {
+  // Emergency Alerts start close on page load
+  // the default behavior is to expand the alerts
+  // Emergency Alerts should stay closed if the cookie is set to false
+
   $('.js-emergency-alerts').each(function(){
     let $el = $(this),
         open = true,
@@ -8,34 +12,29 @@ export default function (window,document,$,undefined) {
         cookieName = 'emergency-alerts' + id,
         cookieValue = cookie.getCookie(cookieName),
         $button = $el.find('.js-accordion-link button'),
-        // Add an aria-expanded attribute with its default value, false. to the <button>
-        $buttonState = $button.attr('aria-expanded', !open);
+        $link = $el.find('.js-accordion-link');
 
-    if(typeof(cookieValue) != 'undefined' && cookieValue === 'false') {
-      // cookieValue is a string so we can't use the value directly
+    // if the user has closed the alerts on a previous page
+    if(typeof(cookieValue) !== 'undefined' && cookieValue === 'false') {
       open = false;
-
+      // set the state of aria-expanded
       $button.attr('aria-expanded', open);
     }
-    if(open) {
-      // expand the menu
-      $el.find('.js-accordion-link').trigger('click');
 
-      $button.trigger('click');
+    // Emergency Alerts loads closed so expand it.
+    if(open) {
+      open = false; // clicking the link swaps the value
+      $link.first().trigger('click');
     }
 
-    $button.on('click', function(e) {
-      e.preventDefault();
+    $link.on('click', function() {
       // toggle the current state
       open = !open;
       // update open/close state cookie
       // leave off third argument to make it expire on session
       cookie.setCookie(cookieName,open);
       // change the state of aria-expanded
-      $buttonState = $buttonState === open ? !open : open;
-      $(this).attr('aria-expanded', $buttonState);
+      $button.attr('aria-expanded', open);
     });
-
   });
-
 }(window,document,jQuery);
