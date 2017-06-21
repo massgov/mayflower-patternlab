b9989269f743fcb9dfeae5e1ae0cc154fbcc61d1
Jonathan Dallas <jonathan.dallas@velir.com>
2017-06-21 14:16:29 -0400

    DP-3911 - Input Number - new styling and JS logic

--- styleguide/source/assets/js/modules/formInputs.js
+++ styleguide/source/assets/js/modules/formInputs.js
@@ -18,4 +18,61 @@ export default function (window,document,$,undefined) {
     });
   });
 
+  // number input type
+  $('input[type="number"]').each(function(){
+    const $el = $(this);
+    const $plus = $('<button type="button" aria-label="increase value" class="ma__input-number__plus"></button>');
+    const $minus = $('<button type="button" aria-label="decrease value" class="ma__input-number__minus"></button>');
+
+    let value = $el.val();
+
+    // restrict the content to numbers only
+    $el.on('keydown', function(e){
+      // Allow: backspace, delete, tab, escape, enter and .
+      if ($.inArray(e.keyCode, [46, 8, 9, 27, 13, 110, 190]) !== -1 ||
+           // Allow: Ctrl/cmd+A
+          (e.keyCode == 65 && (e.ctrlKey === true || e.metaKey === true)) ||
+           // Allow: Ctrl/cmd+C
+          (e.keyCode == 67 && (e.ctrlKey === true || e.metaKey === true)) ||
+           // Allow: Ctrl/cmd+X
+          (e.keyCode == 88 && (e.ctrlKey === true || e.metaKey === true)) ||
+           // Allow: home, end, left, right
+          (e.keyCode >= 35 && e.keyCode <= 39)) {
+               // let it happen, don't do anything
+               return;
+      }
+      // Ensure that it is a number and stop the keypress
+      if ((e.shiftKey || (e.keyCode < 48 || e.keyCode > 57)) && (e.keyCode < 96 || e.keyCode > 105)) {
+          e.preventDefault();
+      }
+    });
+
+    $plus.on('click', function(){
+      let value = parseInt($el.val(),10);
+
+      if(Number.isNaN(value)) {
+        value = 0;
+      }
+
+      $el.val(value + 1);
+    });
+
+    $minus.on('click', function(){
+      let value = parseInt($el.val(),10);
+      
+      if(Number.isNaN(value)) {
+        value = 0;
+      }
+           
+      $el.val(value - 1);
+    });
+
+    // turn of the html5 number feature
+    $el.attr('type','text');
+
+    $el.wrap('<div class="ma__input-number"></div>');
+
+    $el.parent().append($plus,$minus);
+  });
+
 }(window,document,jQuery);
