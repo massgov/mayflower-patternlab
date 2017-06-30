deb91098d2efc942ed28ce70777f37eec3543b62
Jonathan Dallas <jonathan.dallas@velir.com>
2017-06-30 10:52:25 -0400

    DP-3911 - JS form inputs - adding code to handle number pattern

--- styleguide/source/assets/js/modules/formInputs.js
+++ styleguide/source/assets/js/modules/formInputs.js
@@ -18,6 +18,27 @@ export default function (window,document,$,undefined) {
     });
   });
 
+  // number restricted input based on it's pattern (this must run prior to type="number")
+  $('input[type="text"][pattern="[0-9]*"]').on('keydown', function(e){
+    // Allow: delte(46), backspace(8), tab(9), escape(27), enter(13) and space(32))
+    if ($.inArray(e.keyCode, [46, 8, 9, 27, 13, 32]) !== -1 ||
+         // Allow: Ctrl/cmd+A
+        (e.keyCode == 65 && (e.ctrlKey === true || e.metaKey === true)) ||
+         // Allow: Ctrl/cmd+C
+        (e.keyCode == 67 && (e.ctrlKey === true || e.metaKey === true)) ||
+         // Allow: Ctrl/cmd+X
+        (e.keyCode == 88 && (e.ctrlKey === true || e.metaKey === true)) ||
+         // Allow: home, end, left, right
+        (e.keyCode >= 35 && e.keyCode <= 39)) {
+             // let it happen, don't do anything
+             return;
+    }
+    // Ensure that it is a number and stop the keypress
+    if ((e.shiftKey || (e.keyCode < 48 || e.keyCode > 57)) && (e.keyCode < 96 || e.keyCode > 105)) {
+        e.preventDefault();
+    }
+  });
+
   // number input type
   $('input[type="number"]').each(function(){
     const $el = $(this);
@@ -28,7 +49,7 @@ export default function (window,document,$,undefined) {
 
     // restrict the content to numbers only
     $el.on('keydown', function(e){
-      // Allow: backspace, delete, tab, escape, enter and .
+      // Allow: delte(46), backspace(8), tab(9), escape(27), enter(13) and .(110 & 190))
       if ($.inArray(e.keyCode, [46, 8, 9, 27, 13, 110, 190]) !== -1 ||
            // Allow: Ctrl/cmd+A
           (e.keyCode == 65 && (e.ctrlKey === true || e.metaKey === true)) ||
@@ -74,5 +95,6 @@ export default function (window,document,$,undefined) {
 
     $el.parent().append($plus,$minus);
   });
+
 
 }(window,document,jQuery);
