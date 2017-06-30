31cdc51c8bdad2e1117ae4e63111ad5943a7d387
Jonathan Dallas <jonathan.dallas@velir.com>
2017-06-30 14:40:21 -0400

    DP-3911 form page UAT (#496)
    * DP-4233 - Input Text - adding maxlength and pattern fields
    
    * DP-3911 - Tool tip - fixing open and close behavior
    
    * DP-3911 - Labels - font-weight is now 700
    
    * DP-3911 - Input Number - fixing alignment when focused
    
    * DP-3911 - Radio/checkbox - adjusting alignment of input
    
    * DP-3911 - JS form inputs - adding code to handle number pattern
    
    * DP-3911 - Form Page - content updates and new fields
    
    * DP-3911 - Input Number - Leaving as type number to show num keyboard

--- styleguide/source/_patterns/01-atoms/03-forms/input-text.md
+++ styleguide/source/_patterns/01-atoms/03-forms/input-text.md
@@ -23,7 +23,11 @@ Description: An `<input> [type='text']`  element with its corresponding `<label>
     name: 
       type: string/required
     type:
-      type: string ('text','email', etc...) / required
+      type: string ('text','email', 'number', etc...) / required
+    maxlength:
+      type: number / optional
+    pattern:
+      type: string / optional (ex: "[0-9]" for numbers only)
     width:
       type: string (number value) / optional
     placeholder:
--- styleguide/source/assets/js/modules/formInputs.js
+++ styleguide/source/assets/js/modules/formInputs.js
@@ -18,34 +18,57 @@ export default function (window,document,$,undefined) {
     });
   });
 
+  // number restricted input based on it's pattern (this must run prior to type="number")
+  $('input[type="text"][pattern="[0-9]*"]').on('keydown', function(e){
+    // Allow: delte(46), backspace(8), tab(9), escape(27), enter(13) and space(32))
+    if ($.inArray(e.keyCode, [46, 8, 9, 27, 13, 32]) !== -1 ||
+         // Allow: Ctrl/cmd+A
+        (e.keyCode == 65 && (e.ctrlKey === true || e.metaKey === true)) ||
+         // Allow: Ctrl/cmd+C
+        (e.keyCode == 67 && (e.ctrlKey === true || e.metaKey === true)) ||
+         // Allow: Ctrl/cmd+X
+        (e.keyCode == 88 && (e.ctrlKey === true || e.metaKey === true)) ||
+         // Allow: home, end, left, right
+        (e.keyCode >= 35 && e.keyCode <= 39)) {
+             // let it happen, don't do anything
+             return;
+    }
+    // Ensure that it is a number and stop the keypress
+    if ((e.shiftKey || (e.keyCode < 48 || e.keyCode > 57)) && (e.keyCode < 96 || e.keyCode > 105)) {
+        e.preventDefault();
+    }
+  });
+
   // number input type
-  $('input[type="number"]').each(function(){
+  $('input[type="number"], .js-input-number').each(function(){
     const $el = $(this);
     const $plus = $('<button type="button" aria-label="increase value" class="ma__input-number__plus"></button>');
     const $minus = $('<button type="button" aria-label="decrease value" class="ma__input-number__minus"></button>');
 
     let value = $el.val();
 
-    // restrict the content to numbers only
-    $el.on('keydown', function(e){
-      // Allow: backspace, delete, tab, escape, enter and .
-      if ($.inArray(e.keyCode, [46, 8, 9, 27, 13, 110, 190]) !== -1 ||
-           // Allow: Ctrl/cmd+A
-          (e.keyCode == 65 && (e.ctrlKey === true || e.metaKey === true)) ||
-           // Allow: Ctrl/cmd+C
-          (e.keyCode == 67 && (e.ctrlKey === true || e.metaKey === true)) ||
-           // Allow: Ctrl/cmd+X
-          (e.keyCode == 88 && (e.ctrlKey === true || e.metaKey === true)) ||
-           // Allow: home, end, left, right
-          (e.keyCode >= 35 && e.keyCode <= 39)) {
-               // let it happen, don't do anything
-               return;
-      }
-      // Ensure that it is a number and stop the keypress
-      if ((e.shiftKey || (e.keyCode < 48 || e.keyCode > 57)) && (e.keyCode < 96 || e.keyCode > 105)) {
-          e.preventDefault();
-      }
-    });
+    // if the input is not an html input and key restrictions
+    if($el.attr('type') !== "number") {
+      $el.on('keydown', function(e){
+        // Allow: delte(46), backspace(8), tab(9), escape(27), enter(13) and .(110 & 190))
+        if ($.inArray(e.keyCode, [46, 8, 9, 27, 13, 110, 190]) !== -1 ||
+             // Allow: Ctrl/cmd+A
+            (e.keyCode == 65 && (e.ctrlKey === true || e.metaKey === true)) ||
+             // Allow: Ctrl/cmd+C
+            (e.keyCode == 67 && (e.ctrlKey === true || e.metaKey === true)) ||
+             // Allow: Ctrl/cmd+X
+            (e.keyCode == 88 && (e.ctrlKey === true || e.metaKey === true)) ||
+             // Allow: home, end, left, right
+            (e.keyCode >= 35 && e.keyCode <= 39)) {
+                 // let it happen, don't do anything
+                 return;
+        }
+        // Ensure that it is a number and stop the keypress
+        if ((e.shiftKey || (e.keyCode < 48 || e.keyCode > 57)) && (e.keyCode < 96 || e.keyCode > 105)) {
+            e.preventDefault();
+        }
+      });
+    }
 
     $plus.on('click', function(){
       let value = parseInt($el.val().trim(),10);
@@ -67,12 +90,10 @@ export default function (window,document,$,undefined) {
       $el.val(value - 1);
     });
 
-    // turn of the html5 number feature
-    $el.attr('type','text');
-
     $el.wrap('<div class="ma__input-number"></div>');
 
     $el.parent().append($plus,$minus);
   });
+
 
 }(window,document,jQuery);
