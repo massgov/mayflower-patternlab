62cf507ca84ffa78366df515359a846cdc292359
Jes Constantine <jesconstantine@gmail.com>
2017-10-24 15:26:40 -0400

    Make alerts js listeners work on dom load
    - Add empty state scaffolding for emergency alerts org
    - Create handlebars templates for molecules
    - Move logic to ajax alerts JS
    - Hide organism by default

--- styleguide/source/assets/js/modules/ajaxAlerts.js
+++ styleguide/source/assets/js/modules/ajaxAlerts.js
@@ -8,50 +8,72 @@ import getTemplate from "../helpers/getHandlebarTemplate.js";
 export default function (window,document,$,undefined) {
 
   $('.js-ajax-alerts').each(function(){
-    let $el = $(this);
-    let data = {
-      "emergencyAlerts": {
-        "id":"GUID18378923w38789",
-        "buttonAlert": {
-          "hideText": "Hide",
-          "showText": "Show",
-          "text": "Alerts"
-        },
-        "emergencyHeader": {
-          "title": "The State is experiencing severe weather due to the winter storm Paula."
-        },
-        "alerts": [{
-          "message": "MassPort is operating with limited flights due to road closures.",
-          "timeStamp": "02.15.16, 5:00 p.m.",
-          "link": {
-            "href": "#",
-            "text":"Read full alert",
-            "chevron": "true"
-          }
-        },{
-          "message": "The MTBA is operating at a lighter service level, and experiencing delays.",
-          "timeStamp": "02.15.16, 3:00 p.m.",
-          "link": {
-            "href": "#",
-            "text":"Read full alert",
-            "chevron": "true"
-          }
-        },{
-          "message": "The Governor directs non-emergency executive branch state employees to not come in to work.",
-          "timeStamp": "02.13.16, 10:00 a.m.",
-          "link": {
-            "href": "#",
-            "text":"Read full alert",
-            "chevron": "true"
-          }
-        }]
-      }
-    };
-
-    if (data.emergencyAlerts.alerts.length || data.emergencyAlerts.emergencyHeader) {
-      let compiledTemplate = getTemplate("emergencyAlerts");
-      let markup = compiledTemplate(data);
-      $el.html(markup);
-    }
+    let alertsEndpoint = '/assets/data/emergency-alerts.json';
+    updateAlerts.apply(this, [alertsEndpoint]);
+    $(this).removeClass('ma__ajax-alerts--no-alerts');
   });
+
+  function renderAlerts(alertsData) {
+    if (alertsData.emergencyAlerts.alerts.length || alertsData.emergencyAlerts.emergencyHeader) {
+      // Header
+      let header = alertsData.emergencyAlerts.emergencyHeader;
+      if (header) {
+        let compiledTemplate = getTemplate("emergencyHeader");
+        let markup = compiledTemplate(header);
+        $(this).find('.js-ajax-alerts-header-content').replaceWith(markup);
+      }
+
+      // Buttons
+      let button = alertsData.emergencyAlerts.buttonAlert;
+      if (button) {
+        let compiledTemplate = getTemplate("buttonAlert");
+        let markup = compiledTemplate(button);
+        $(this).find('.js-accordion-link').each(function(){
+          $(this).html(markup);
+        });
+      }
+
+      // Alerts
+      let alerts = alertsData.emergencyAlerts.alerts;
+      if (alerts.length) {
+        let compiledTemplate = getTemplate("emergencyAlert");
+        let markup = '';
+        alerts.forEach(function(alert){
+          markup += compiledTemplate(alert);
+        });
+        $('.js-ajax-alerts-content').html(markup);
+      }
+    }
+  }
+
+  function getAlertsData(endpoint) {
+    // Keep .js-ajax-alerts object.
+    let that = this;
+    let promise = $.Deferred();
+
+    $.ajax({
+      type: 'GET',
+      url: endpoint,
+      cache: false,
+      dataType: 'json'
+    }).done(function(data){
+      // @todo validate data against schema
+      promise.resolve(data, that);
+    }).fail(function(jqXHR, textStatus, errorThrown) {
+      console.log(textStatus);
+      promise.reject()
+    });
+
+    return promise;
+  }
+
+  function updateAlerts(endpoint) {
+    getAlertsData.apply(this, [endpoint])
+    .done(function(alertsData, that){
+      renderAlerts.apply(that, [alertsData]);
+    })
+    .fail(function(){
+      console.log('fail fail fail');
+    });
+  }
 }(window,document,jQuery);
