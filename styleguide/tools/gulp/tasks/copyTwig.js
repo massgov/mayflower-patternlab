9a9f45bff4b97bf71dd79320307e5f9066aaa55d
Jes Constantine <jesconstantine@gmail.com>
2017-11-15 09:39:51 -0500

    Copy twig templates into /public on gulp build
    So we can access them via ajax to render clientside

---
+++ styleguide/tools/gulp/tasks/copyTwig.js
@@ -0,0 +1,27 @@
+var gulp   = require("gulp"),
+    quench = require("../quench.js"),
+    debug  = require("gulp-debug");
+
+module.exports = function copyTwigTask(config, env){
+
+    // copy files settings
+    var twig = {
+        src: [
+            config.patternLabRoot + "/source/_patterns/**/*.*"
+        ],
+        dest: config.dest + "/assets"
+    };
+
+    // register the watch
+    quench.registerWatcher("copyTwig", twig.src);
+
+
+    /* copy files */
+    gulp.task("copyTwig", function(next) {
+
+        return gulp.src(twig.src, { base: config.root })
+            .pipe(quench.drano())
+            .pipe(gulp.dest(twig.dest))
+            .pipe(debug({title: "copyTwig:"}));
+    });
+};
