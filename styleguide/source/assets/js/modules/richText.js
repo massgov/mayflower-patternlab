62dbd84b847a101c88bac2097e3f55b3733f2b66
Jayme Johnson <jayme@lastcallmedia.com>
2017-11-27 14:13:49 -0600

    Jess: Use classes and data attribute to indent rich text content. DP-5817

--- styleguide/source/assets/js/modules/richText.js
+++ styleguide/source/assets/js/modules/richText.js
@@ -5,16 +5,12 @@ export default function (window,document,$,undefined) {
 
     $el.find('table').wrap( "<div class='ma__rich-text__table-wrapper'></div>" );
 
+    // Allow heading and contents to be indented.
     if ($el.parents('.js-outline-indent').length) {
-      let indent = {
-        'H2': 20,
-        'H3': 40,
-        'H4': 60,
-        'H5': 80,
-        'H6': 100,
-      };
       $el.find(':header').each(function(index,header){
-        $(header).nextUntil(':header').addClass("ma__rich-text__indent" + indent[$(header).prop('tagName')]);
+        $(header).nextUntil(':header')
+          .addClass("ma__rich-text__indent")
+          .attr('data-ma-heading-parent', $(header).prop('tagName'));
       });
     }
   });
