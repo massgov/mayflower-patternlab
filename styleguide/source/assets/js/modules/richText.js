dd4ac9bb81f72679e461326d217c5f64197936f5
Jes Constantine <jesconstantine@gmail.com>
2017-12-01 14:35:56 -0500

    Jess: Use js to help target rich text content for indentation. DP-5817

--- styleguide/source/assets/js/modules/richText.js
+++ styleguide/source/assets/js/modules/richText.js
@@ -4,5 +4,18 @@ export default function (window,document,$,undefined) {
     let $el = $(this);
 
     $el.find('table').wrap( "<div class='ma__rich-text__table-wrapper'></div>" );
+
+    if ($el.parents('.js-outline-indent').length) {
+      let indent = {
+        'H2': 20,
+        'H3': 40,
+        'H4': 60,
+        'H5': 80,
+        'H6': 100,
+      };
+      $el.find(':header').each(function(index,header){
+        $(header).nextUntil(':header').addClass("ma__rich-text__indent" + indent[$(header).prop('tagName')]);
+      });
+    }
   });
 }(window,document,jQuery);
