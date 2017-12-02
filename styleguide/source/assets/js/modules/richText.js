5b746fd58400733852741ed6ea007842ddb7a38b
Jayme Johnson <jayme@lastcallmedia.com>
2017-12-01 19:51:42 -0500

    Make js selector target .js-outline-indent instead of its parent.

--- styleguide/source/assets/js/modules/richText.js
+++ styleguide/source/assets/js/modules/richText.js
@@ -6,7 +6,7 @@ export default function (window,document,$,undefined) {
     $el.find('table').wrap( "<div class='ma__rich-text__table-wrapper'></div>" );
 
     // Allow heading and contents to be indented.
-    if ($el.parents('.js-outline-indent').length) {
+    if ('.js-outline-indent'.length) {
       $el.find(':header').each(function(index,header){
         $(header).nextUntil(':header')
           .addClass("ma__rich-text__indent")
