a2271c9c34ab655a80cb97902744e63c484c6cec
devclair <clair.smith@mediacurrent.com>
2018-03-01 08:58:36 -0500

    DP-7766: move styles to inner wrapper so empty wrapper doesn't push page down

--- styleguide/source/assets/js/modules/stickyTOC.js
+++ styleguide/source/assets/js/modules/stickyTOC.js
@@ -1,18 +1,32 @@
 export default function (window,document,$,undefined) {
 
-  // const $pageContent = $('#main-content .ma__information-details__content');
-  // const tocSections = $pageContent.find('h2').length;
 
-  const $toc = $('.ma__sticky-toc__links');
-  const tocSections = $('.ma__sticky-toc__link').length;
 
-  if (tocSections > 3) {
-    // insert Table of Content
-  }
 
-  if (tocSections > 10) {
 
-  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
   $('.ma__sticky-toc__toggle-link').on('click', function() {
     $toc.toggleClass('is-open');
