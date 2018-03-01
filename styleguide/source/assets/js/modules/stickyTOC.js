b98324dad31b56bbeaf54bb99a95b8970763406a
devclair <clair.smith@mediacurrent.com>
2018-03-01 10:33:54 -0500

    DP-7766: add links to TOC from content

--- styleguide/source/assets/js/modules/stickyTOC.js
+++ styleguide/source/assets/js/modules/stickyTOC.js
@@ -1,35 +1,31 @@
 export default function (window,document,$,undefined) {
 
+  var $toc = $('.ma__sticky-toc');
+  var $tocContent = $('.ma__sticky-toc__links');
+  var $tocSections = $('.ma__information-details__content').find('h2');
+  var tocSectionCount = $tocSections.length;
 
+  if (tocSectionCount > 3) {
+    $toc.show();
+  }
 
+  if (tocSectionCount > 10) {
+    if ($(window).width() > 558 ) {
+      $('.ma__sticky-toc__footer').show();
+    }
+  }
 
+  $tocSections.each(function() {
+    var $section = $(this);
+    var sectionId = $section.attr('id');
+    var sectionTitle = $section.text();
+    var $tocLink = '<div class="ma__sticky-toc__link"><a href="#'+sectionId+'" >'+sectionTitle+'</a></div>'
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    $('.ma__sticky-toc__column').append($tocLink);
+  });
 
   $('.ma__sticky-toc__toggle-link').on('click', function() {
-    $toc.toggleClass('is-open');
+    $tocContent.toggleClass('is-open');
   });
 
   $('.ma__sticky-toc__footer').on('click', function() {
@@ -40,7 +36,7 @@ export default function (window,document,$,undefined) {
   });
 
   $(window).resize(function () {
-    $('.ma__sticky-toc__link, .ma__sticky-toc__footer').removeAttr('style');
+    $('.ma__sticky-toc__link').removeAttr('style');
   }).resize();
 }
 (window,document,jQuery);
