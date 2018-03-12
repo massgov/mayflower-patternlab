9bf67ca9e97182c73b1b61a15d4b6cfa2cfb6228
Isaac Chansky <ichansky@gmail.com>
2018-03-12 16:35:03 -0400

    update transition for table of contents

--- styleguide/source/assets/js/modules/inlineOverlay.js
+++ styleguide/source/assets/js/modules/inlineOverlay.js
@@ -4,39 +4,37 @@ export default function (window,document,$,undefined) {
   let containerClass = '.js-inline-overlay';
   let contentClass = '.js-inline-overlay-content';
   let toggleClass = '.js-inline-overlay-toggle';
+  let titleClass  = '.js-inline-overlay-title';
 
-  function toggleOverlay() {
-    let $toggleEl = $(this);
-    let $containerEl = $toggleEl.parents(containerClass);
-    let $contentEl = $toggleEl.parents(containerClass).find(contentClass);
-    let isOpen = $containerEl.hasClass('is-open');
-    $('body').toggleClass('scroll-disabled', !isOpen);
-    // if on mobile & we're closing the overlay, scroll title back into view
-    if (document.documentElement.clientWidth < 840 && isOpen) {
-      $containerEl.toggleClass('is-open', !isOpen);
-      setTimeout(function() {
-        $('html, body').animate({
-          scrollTop: $containerEl.offset().top - 80
-          });
-      }, 250);
-
-    } else {
-      $('html, body').animate({
-        scrollTop: $containerEl.offset().top
-      }, 250, function() {
-        $containerEl.toggleClass('is-open', !isOpen);
-      });
-    }
-
+  function initialize () {
     // Add random ID if no ID present.
-    let contentID = $contentEl.attr('id');
+    let contentID = $(contentClass).attr('id');
     if(!contentID) {
-       $containerEl.attr('id', `overlay-${Math.floor(Math.random()*100000)}`);
+      let id = `overlay-${Math.floor(Math.random()*100000)}`;
+       $(containerClass).attr('id', id);
+       $(toggleClass).attr('aria-controls, id');
     }
-    // update aria
-    $toggleEl.attr('aria-expanded',!isOpen).attr('aria-controls', contentID);
   }
 
+  function toggleOverlay() {
+    let $containerEl = $(containerClass);
+    let $contentEl = $(contentClass);
+    let isOpen = $containerEl.hasClass('is-open');
+    $('body').toggleClass('scroll-disabled', !isOpen);
+    $containerEl.toggleClass('is-open', !isOpen);
+
+    $(`${toggleClass}[aria-expanded=${!isOpen}]`).parents(titleClass).focus();
+  }
+
+  initialize();
   $(document).on('click', toggleClass, toggleOverlay);
+  // allow esc key to dismiss overlay
+  $(document).keydown(function(e) {
+    // ESCAPE key pressed
+    if (e.keyCode === 27 && $(containerClass).hasClass('is-open')) {
+      toggleOverlay();
+    }
+  });
+
 
 }(window,document,jQuery);
