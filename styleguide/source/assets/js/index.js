a71c4aac8117446987c6a2af529e87825c25c71f
Isaac Chansky <ichansky@gmail.com>
2018-03-22 12:16:58 -0400

    DP 7767 binder page navigation (#710)
    * add basic hierarchy table of contents
    
    * add inverted illustrator header variant
    
    * add binder page content type
    
    * add to binder page content type, add documentation
    
    * remove jump links example for this PR
    
    * fix padding within toc
    
    * fix padding on downloads within table of contents
    
    * add twig block mainContent
    
    This will allow developers to override the RichText component.
    
    * fix typo
    
    * link example for listing table
    
    * ul instead of div, fix sass partial missing _
    
    * ul instead of div, fix sass partial missing _
    
    * WIP working on table of contents collapsable organism
    
    * Add table of contents overlay organism
    
    * better control page scrolling when overlay is open, adjust spacing/alignment in overlay toggle, add specific example content
    
    * add optional page flipper context
    
    * add current page indication
    
    * update docs for toc hierarchy to include currentpage flag
    
    * spacing adjustments
    
    * spacing/sizing adjustments
    
    * make button separate from title link
    
    * smoothing out animations
    
    * update transition for table of contents
    
    * adjust spacing on toc title
    
    * TOC block to override and js limit scope (#736)
    
    The implementation of the TOC Overlay needed a block to be
    overridden so that we could use the same variable mapping as the
    normal display of the TOC. Because multiple navigation components
    may be implemented, the overlay block needed to be limited to the
    closest implementation instead of the first.
    
    * remove unnecessary css
    
    * adjust what scrolls
    
    * fix wrapping in firefox

--- styleguide/source/assets/js/index.js
+++ styleguide/source/assets/js/index.js
@@ -14,6 +14,7 @@ import footnote                   from "./modules/footnote.js";
 import formValidation             from "./modules/formValidation.js";
 import headerTags                 from "./modules/headerTags.js";
 import hideAlert                  from "./modules/hideAlert.js";
+import inlineOverlay              from "./modules/inlineOverlay.js";
 import imageFill                  from "./modules/imageFill.js";
 import keywordSearch              from "./modules/keywordSearch.js";
 import locationFilters            from "./modules/locationFilters.js";
