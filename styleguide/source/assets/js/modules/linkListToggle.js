3d6f747bbfc6ecb73ff91d0e856c5c1c0489f0c0
devclair <clair.smith@mediacurrent.com>
2018-04-09 11:21:28 -0400

    DP-8517: testing updated and cleaner css

--- styleguide/source/assets/js/modules/linkListToggle.js
+++ styleguide/source/assets/js/modules/linkListToggle.js
@@ -4,23 +4,31 @@ export default function (window,document,$,undefined) {
 
     const $thisList = $(this).find('.ma__link-list__items'),
     $thisToggle = $(this).find('.ma__link-list__toggle'),
+    $thisToggleLabel = $thisToggle.find('span'),
     $listItem = $thisList.children(),
     listItems = $listItem.length;
+
     let width = $(window).width();
 
     if ((listItems > 3) && (width < 1201)) {
       // Show More button only when there are enough to toggle.
-      $thisToggle.toggle();
+      $thisToggle.addClass('show-toggle');
     }
 
     $thisToggle.on('click', function() {
+      let thisToggleText = $thisToggleLabel.text();
+
+      $thisToggle.toggleClass('toggle-open');
+      $thisToggleLabel.text(thisToggleText === "less" ? "more" : "less");
+
+      // Because the heights are dynamic a css transition would be buggy.
       // Toggle all after first 3 on mobile.
       if (width < 780) {
-        $listItem.eq(3).nextAll().slideToggle();
+        $listItem.eq(3).nextAll().slideToggle().toggleClass('toggle-open');
       }
       // Toggle all after first 6 on tablet.
       else {
-        $listItem.eq(6).nextAll().slideToggle();
+        $listItem.eq(6).nextAll().slideToggle().toggleClass('toggle-open');
       }
     });
 
