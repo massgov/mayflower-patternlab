4977b2cd0bfb4da56c20887aa881fd787fe61211
Rob Bayliss <rob@lastcallmedia.com>
2018-04-18 14:13:53 -0400

    Reset threshold

--- backstopjs/backstop.js
+++ backstopjs/backstop.js
@@ -26,7 +26,6 @@ var scenarios = files.map(function(file) {
     return {
         label: path.basename(file, '.html').replace('05-pages-', 'page-').replace('04-templates-', 'template-').replace('-', ' '),
         url: `http://web/patterns/${path.relative(patternPath, file)}`,
-        misMatchThreshold: 0.05
     }
 });
 
